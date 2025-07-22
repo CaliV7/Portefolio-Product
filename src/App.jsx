ee055d16f417c5d7cb91459c853dcf0654364dc2
Calista Volget <calistavolget@gmail.com>
2025-07-22 11:06:39 +0200

    Initialize Vite + React + Tailwindcss setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './index.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white text-black flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Bienvenue dans le Portfolio</h1>
+    </div>
+  );
+}
+
+export default App;
